0b4dd5821e7379b623c64e212342e15bf0d51765
Sourajit <sourajitmandal19@gmail.com>
2023-11-13 21:22:15 +0530

    create first part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Navigation from './Navigation';
+import Home from './pages/Home';
+import Login from './pages/Login';
+// import Navbar from './components/Navbar.jsx'
+import Patelstudent from './pages/Patelstudent';
+import Tilakstudent from './pages/Tilakstudent';
+import Patelgallery from "./pages/patelstudent/Patelgallery"
+import Patelcomment from './pages/patelstudent/Patelcomment';
+import Patelfullmenu from './pages/patelstudent/Patelfullmenu';
+import Cheifwarden from './pages/Cheifwarden';
+import Accountant from "./pages/Accountant";
+import Patelallcomplaint from "./pages/patelstudent/Patelallcomplaint";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <Router>
+        {/* <Navigation /> */}
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/patelstudent" element={<Patelstudent />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/patelgallery" element={<Patelgallery />} />
+          <Route path="/patelcomment" element={<Patelcomment />} />
+          <Route path="/patelfullmenu" element={<Patelfullmenu />} />
+          <Route path="/patelallcomplaint" element={<Patelallcomplaint />} />
+
+          <Route path="/tilakstudent" element={<Tilakstudent />} />
+
+          <Route path="/cheifwarden" element={<Cheifwarden />} />
+
+          <Route path="/accountant" element={<Accountant />} />
+        </Routes>
+      </Router>
+    );
+  }
 
 export default App;
